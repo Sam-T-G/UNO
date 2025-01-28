7ae291f62aa22a77118426cdad3463ad30c7512c
Sam.G <110798400+Sam-T-G@users.noreply.github.com>
2025-01-28 12:59:09 -0800

    Create uno.cpp

---
+++ uno.cpp
@@ -0,0 +1,34 @@
+/*
+Name:
+Date:
+Purpose:
+*/
+
+// System Libraries
+#include <iostream>
+using namespace std;
+
+// User Libraries
+
+// Global Constants
+
+// Only use scientific values like pi, speed of light, etc
+
+// Conversion between units
+
+// Function Protypes
+int main(int argv, char **argc)
+{
+    // Random seeds are set for random number generator
+
+    // Declare Variables
+
+    // Initialize Variables
+
+    // Map the inputs and outputs - Process
+
+    // Display and output the results
+
+    // Exit the program
+    return 0;
+}
